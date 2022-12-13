25e57d3f40cd9e156ecdff37dac0694bc66e4e27
Jojo3235 <joralmand@gmail.com>
2022-12-13 11:43:10 +0100

    ej2 y ej4

---
+++ ejercicio2.py
@@ -0,0 +1,45 @@
+#crear diccionario con clave valor de los numeros del 1 al 9
+
+numeros = {'uno': 1, 'dos':2, 'tres':3, 'cuatro':4, 'cinco':5, 'seis':6, 'siete':7, 'ocho':8, 'nueve': 9}
+
+def uno_num():
+    return numeros['uno']
+
+def dos_num():
+    return numeros['dos']
+
+def tres_num():
+    return numeros['tres']
+
+def cuatro_num():
+    return numeros['cuatro']
+
+def cinco_num():
+    return numeros['cinco']
+
+def seis_num():
+    return numeros['seis']
+
+def siete_num():
+    return numeros['siete']
+
+def ocho_num():
+    return numeros['ocho']
+
+def nueve_num():
+    return numeros['nueve']
+
+def main():
+    print('Puedo escribir números como {},{},{}'.format(uno_num(),dos_num(),tres_num()))
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
--- ejercicio4.py
+++ ejercicio4.py
@@ -1,3 +1,27 @@
 def pedir_numero():
     cadena = input('Introduce una cadena de caracteres: ')
     return cadena
+
+def lucky_ticket():
+    numero = pedir_numero()
+    if len(numero) % 2 == 0:
+        numero1 = numero[:len(numero)//2]
+        numero2 = numero[len(numero)//2:]
+        suma1 = sum(numero1)
+        suma2 = sum(numero2)
+        if suma1 == suma2:
+            return True
+        else:
+            return False
+    else:
+        numero1 = numero[:(len(numero)+1)//2]
+        numero2 = numero[(len(numero)-1)//2:]
+        suma1 = sum(numero1)
+        suma2 = sum(numero2)
+        if suma1 == suma2:
+            return True
+        else:
+            return False
+        
+def main():
+    print(lucky_ticket())
