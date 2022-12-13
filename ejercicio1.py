1bf1271caea58b6be7451651fd92ed7e9ee02c44
Jojo3235 <joralmand@gmail.com>
2022-12-13 20:33:05 +0100

    ej1 completado

--- ejercicio1.py
+++ ejercicio1.py
@@ -12,15 +12,27 @@ def lista_a_string(palabra):
     palabra2 = ' '.join(palabra)
     return palabra2
 
+def index_no_nulos_lista(lista):
+    for elemento in lista:
+        if elemento != '':
+            return lista.index(elemento)
+
 def main():
     palabra = input('Ingrese una palabra: ')
-    palabra = separar_kword(palabra)
-    if '' in palabra[:1]:
-        print('No se puede ingresar un # en medio del mensaje') 
+    if palabra[0] == '#':
+        palabra = separar_kword(palabra)
+        index = index_no_nulos_lista(palabra)
+        if len(palabra[index:])>1:
+            print(list())
+        else:
+            palabra = sacar_mensaje(palabra)
+            palabra1 = lista_a_string(palabra)
+            if palabra1[0].isalpha() == True:
+                print(palabra1)
+            else:
+                print('No se pueden escribir caracteres especiales o numericos al inicio del mensaje')
     else:
-        palabra = sacar_mensaje(palabra)
-        palabra = lista_a_string(palabra)
-        print(palabra)
+        print('El mensaje ha de empezar por #')
 
 if __name__ == '__main__':
     main()
